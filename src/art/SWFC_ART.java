4eac800dd4fac24b358216e62ed02a685fc2b896
邓僚僚 <181250025@smail.nju.edu.cn>
2022-11-29 14:01:16 +0800

    1

---
+++ src/art/SWFC_ART.java
@@ -0,0 +1,21 @@
+package art;
+
+import util.Testcase;
+
+public class SWFC_ART extends AbstractART{
+
+    public static void main(String args[]){
+        //TODO:能单独运行该算法的入口
+    }
+
+    @Override
+    public Testcase bestCandidate() {
+        //TODO:找到下一个测试用例的方法
+        return null;
+    }
+
+    @Override
+    public void testEfficiency(int pointNum) {
+        //TODO:生成指定数量测试用例的方法
+    }
+}
